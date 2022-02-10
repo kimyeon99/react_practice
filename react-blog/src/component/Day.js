c70d7960fd9f843627dc28556997ead0af1d9690
예승재 <tmdwo7017@naver.com>
2022-02-10 23:53:59 +0900

    react practice - wordlist

--- react-blog/src/component/Day.js
+++ react-blog/src/component/Day.js
@@ -1,23 +1,10 @@
-import { useEffect, useState } from "react";
+import dummy from "../db/data.json"
 import { useParams } from "react-router-dom";
 import Word from "./Word"
 
 export default function Day() {
-
     const { day } = useParams();
     //const day = useParams().day;
-
-    const [words, setWords] = useState([]);
-
-    useEffect(() => {
-        fetch(`http://localhost:3001/words?day=${day}`)
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setWords(data);
-            })
-    }, [day]);
 
     const wordList = dummy.words.filter(word => (
         word.day == day
@@ -28,7 +15,7 @@ export default function Day() {
             <table>
                 <h2>Day {day}</h2>
                 <tbody>
-                    {words.map(word => (
+                    {wordList.map(word => (
                         <Word word={word} key={word.id} />
                     ))}
                 </tbody>
